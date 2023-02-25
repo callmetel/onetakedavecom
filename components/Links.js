7a1cb263d6dc42f2853a400834f1d33ba45a5e3a
Tel <tel@jsdfyodds.com>
2023-02-25 18:38:05 -0500

    fix dependencies

--- components/Links.js
+++ components/Links.js
@@ -4,31 +4,31 @@ import { motion } from "framer-motion";
 const Links = () => (
   <motion.div className="grid">
     <Link href="/docs">
-      <a className="card">
+      <span className="card">
         <h2>Documentation &rarr;</h2>
         <p>Find in-depth information about Next.js features and API.</p>
-      </a>
+      </span>
     </Link>
 
     <Link href="/learn">
-      <a className="card">
+      <span className="card">
         <h2>Learn &rarr;</h2>
         <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
+      </span>
     </Link>
 
     <Link href="/examples">
-      <a className="card">
+      <span className="card">
         <h2>Examples &rarr;</h2>
         <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
+      </span>
     </Link>
 
     <Link href="/deploy">
-      <a className="card">
+      <span className="card">
         <h2>Deploy &rarr;</h2>
         <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      </span>
     </Link>
   </motion.div>
 );
