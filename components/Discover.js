1c9ebecafa3402806248382b6ded13a09051be6d
Tel <tel@jsdfyodds.com>
2023-07-16 21:28:39 -0400

    added discover module sequential animations

--- components/Discover.js
+++ components/Discover.js
@@ -1,51 +1,61 @@
-import { motion } from "framer-motion";
+import { motion, stagger, animate, useAnimate } from "framer-motion";
+import { useRef, useEffect } from "react";
+
+const useDiscoverAnimation = (isOpen) =>
+{
+	const [scope, animate] = useAnimate();
+	const OpeningSequence = [
+		[scope.current, { y: ["100%", "0%"] }, { duration: .3, ease: "easeInOut" }],
+		[".DiscoverContent", { background: ["#fff", "#000"] }, { duration: 0.1, ease: "easeInOut" }],
+		[scope.current, { scaleX: [0.001, 1] }, { duration: .4, at: "<", ease: "easeInOut" }],
+		[".HeaderImg img", { opacity: [0, 1], y: ["-20%", "0%"] }, { duration: 0.75, delay: stagger(0.2), ease: "easeInOut", at: 0.75 }],
+		[".CloseDiscover .line", { scaleX: [0, 1], transformOrigin: "left" }, { duration: 0.3, ease: "easeInOut", delay: -0.75 }],
+		[".CloseDiscover .text", { opacity: [0, 1] }, { duration: 0.5, ease: "easeInOut", delay: -0.5 }],
+	];
+
+	const ClosingSequence = [
+		[".CloseDiscover .text", { opacity: [1, 0] }, { duration: 0.4, ease: "easeInOut" }],
+		[".CloseDiscover .line", { scaleX: [1, 0], transformOrigin: "left" }, { duration: 0.3, ease: "easeInOut", at: "<" }],
+		[".HeaderImg img", { opacity: [1, 0], y: ["0%", "-20%"] }, { duration: 0.75, delay: stagger(0.2, { from: "last" }), ease: "easeInOut" }],
+		[scope.current, { scaleX: [1, 0.001] }, { duration: .4, ease: "easeInOut" }],
+		[".DiscoverContent", { background: ["#000", "#fff"] }, { duration: 0.001, ease: "easeInOut" }],
+		[scope.current, { y: ["0%", "100%"] }, { duration: .3, at: "<", ease: "easeInOut" }],
+	];
+
+	useEffect(() =>
+	{
+		if (isOpen)
+		{
+			animate(OpeningSequence);
+		}
+		else
+		{
+			animate(ClosingSequence);
+		}
+	}, [isOpen]);
+	return scope;
+}
 
 const Discover = (props) =>
 {
 	// console.log(props.isOpen);
-	const OpenCloseAnimations = props.isOpen ?
-		{
-			y: 0,
-			scaleX: 1,
-			transformOrigin: "top"
-		}
-		: {
-			y: "100%",
-			scaleX: 0.001,
-			transformOrigin: "top"
-		};
-	const OpenCloseTransitions = props.isOpen ?
-		{
-			y: { duration: .3 },
-			scaleX: { duration: .4, delay: .4 },
-		}
-		: props.routeChanged ?
-			{
-				y: { duration: 0 },
-				scaleX: { duration: 0 },
-			}
-			: {
-				y: { duration: .3, delay: .5 },
-				scaleX: { duration: .4 },
-			};
-
+	const scope = useDiscoverAnimation(props.isOpen);
 	const closeDiscover = () =>
 	{
 		props.discoverCallback(false);
 	};
 	return (
-		<motion.div layout
-			animate={OpenCloseAnimations}
-			transition={OpenCloseTransitions}
+		<div
+			ref={scope}
 			className="Discover"
 			data-state={props.state}>
-			<div className="DiscoverContent">
-				<button className="CloseDiscover" onClick={closeDiscover}>Back</button>
-				<div class="Inner">
+			<div className="DiscoverContent" ref={props.forwardedRef}>
+				<button className="CloseDiscover" onClick={closeDiscover}><span className="line"></span><span className="text">Back</span></button>
+				<div className="Inner">
 					{props.children}
 				</div>
 			</div>
-		</motion.div>
+		</div>
 	);
 };
 export default Discover;
--- pages/who.js
+++ pages/who.js
@@ -6,6 +6,7 @@ import Discover from "../components/Discover";
 import { Button, DiscoverButton } from "../components/Buttons";
 import { BikeRoute } from "/components/BikeRoute";
 import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 const Who = (props) =>
 {
@@ -25,6 +26,8 @@ const Who = (props) =>
 		// console.log(data);
 		setState(data);
 	}
+	const DiscoverContentRef = useRef(null);
+
 
 	return (
 		<div className="page" ref={pageRef} data-page={props.location.state}>
@@ -40,19 +43,33 @@ const Who = (props) =>
 					/>
 					<DiscoverButton click={() => { handleDiscover(true) }} />
 				</div>
-				<Discover state={props.location.state} routeChanged={props.routeChanged} isOpen={isOpen} discoverCallback={handleDiscover}>
+				<Discover forwardedRef={DiscoverContentRef} state={props.location.state} routeChanged={props.routeChanged} isOpen={isOpen} discoverCallback={handleDiscover}>
 					<div className="HeaderImg">
 						<img src={`${img}who-1.jpg`} className="mainImg" />
 						<img src={`${img}who-2.png`} className="titleText" />
 						<img src={`${img}who-3.png`} className="signature" />
 						<img src={`${img}who-shadow-1.png`} className="shadow" />
 					</div>
-					<div className="Headline">Born with DJ&nbsp;Disco Dave as a father,</div>
-					<div className="Paragraph"><p>OneTakeDave had exclusive access to classic soul and funk music, as well as a light knowledge of sound blending and electronic mixing equipment. Keeping it all in the family, the senior Dave was exposed to the very beginnings of DJ culture when his mother would travel between Allentown, PA and Philadelphia, breaking all the latest records of the city’s hottest musicians at her house parties. As a precocious and highly observant child, OneTakeDave began to note the mood-changing power of the DJ, and he began to carry on the family tradition and embrace the elements of the lifestyle, eventually wanting to practice DJ’ing stint before focusing his talents on rap.</p></div>
-					<div className="FooterImg">
+					<motion.div
+						className="Headline"
+						initial={{ opacity: 0, y: "20%" }}
+						whileInView={{ opacity: 1, y: "0%", transition: { delay: 0.25, duration: 1.5, ease: "easeInOut" } }}
+						/*viewport={{ root: DiscoverContentRef, once: false }}*/>Born with DJ&nbsp;Disco Dave as a father,</motion.div>
+					<motion.div
+						className="Paragraph"
+						initial={{ opacity: 0, y: "20%" }}
+						whileInView={{ opacity: 1, y: "0%", transition: { delay: 0.25, duration: 1, ease: "easeInOut" } }}
+						viewport={{ root: DiscoverContentRef, once: false }}>
+						<p>OneTakeDave had exclusive access to classic soul and funk music, as well as a light knowledge of sound blending and electronic mixing equipment. Keeping it all in the family, the senior Dave was exposed to the very beginnings of DJ culture when his mother would travel between Allentown, PA and Philadelphia, breaking all the latest records of the city’s hottest musicians at her house parties. As a precocious and highly observant child, OneTakeDave began to note the mood-changing power of the DJ, and he began to carry on the family tradition and embrace the elements of the lifestyle, eventually wanting to practice DJ’ing stint before focusing his talents on rap.</p>
+					</motion.div>
+					<motion.div
+						className="FooterImg"
+						initial={{ opacity: 0, y: "20%", x: "0vw" }}
+						whileInView={{ opacity: 1, y: "0%", x: "0vw", transition: { delay: 0.25, duration: 1, ease: "easeInOut" } }}
+						viewport={{ root: DiscoverContentRef, once: false }}>
 						<img src={`${img}who-4.jpg`} />
 						<img src={`${img}who-shadow-2.png`} className="shadow" />
-					</div>
+					</motion.div>
 				</Discover>
 				<BikeRoute state={props.location.state} time={route.time} duration={route.duration} popstate={props.popstate} clicked={props.routeChanged} />
 			</main>
